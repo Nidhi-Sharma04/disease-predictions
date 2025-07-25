167fb64eda0437dab5768c7de085a8415eec888f
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-07-25 18:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from streamlit_option_menu import option_menu
 
 # Set the page title and icon
 st.set_page_config(page_title="Disease Prediction", page_icon="🧑‍⚕️", layout="centered")
-st.markdown("""<style>section[data-testid="stSidebar"] > div:first-child {background-color: #CCD2E8;}</style>""", unsafe_allow_html=True)
+st.markdown("""<style>section[data-testid="stSidebar"] > div:first-child {background-color: #1C4161;}</style>""", unsafe_allow_html=True)
 
 # Function to set background image
 def set_background(image_file):
